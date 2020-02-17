41f2d3917770f8a9f05ece79d68ca3fa9f8b9fc6
Silvia Patono <silvia.patono@gmail.com>
2020-02-17 23:57:26 +0100

    full solution drawing the routes manually

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  # Sono obbligata a mettere la show dopo new perche altrimenti errore id='new'
+  # not found
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
+end
